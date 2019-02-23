93b753c59caba3063bbdc13271a61a95d5ce15cf
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 00:31:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
 def oxford_comma(array)
-  if array.length = 1
+  if array.length == 1
     array[0]
-  elsif array.length = 2
+  elsif array.length == 2
     puts "#{array[0]} and #{array[1]}"
   elsif array.length > 2
     last = array.pop
