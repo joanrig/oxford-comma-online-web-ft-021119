38c00549b89d9a438d9caf8b29fc5ff5e91ff994
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 00:38:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,14 @@
 require 'pry'
 
 def oxford_comma(array)
-  binding.pry
   if array.length == 1
     array[0]
   elsif array.length == 2
     array[0] + " and " + array[1]
   elsif array.length > 2
     last = array.pop
-    list = array.map { |word| word + ", "}
+    list = array.join(", ")
     list + ",  and" + last
-
+    bining.pry
   end
 end
