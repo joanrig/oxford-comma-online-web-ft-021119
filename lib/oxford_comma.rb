ee727535dddcf560f93090670a72993d7b8ba87f
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 00:31:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   if array.length == 1
     array[0]
   elsif array.length == 2
-    puts "#{array[0]} and #{array[1]}"
+    return array[0] + " and " + array[1]
   elsif array.length > 2
     last = array.pop
     list = array.map { |word| word + ", "}
