cca52ee045d4026a715d040a06a1138e324781d1
Joan Lyness <joanrigdon@gmail.com>
2019-02-23 00:30:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,13 @@
 require 'pry'
-def oxford_comma(array)
-  array[0]
-end
 
 def oxford_comma(array)
-  puts "#{array[0]} and #{array[1]}"
+  if array.length = 1
+    array[0]
+  elsif array.length = 2
+    puts "#{array[0]} and #{array[1]}"
+  elsif array.length > 2
+    last = array.pop
+    list = array.map { |word| word + ", "}
+    return list + ", " + last
+  end
 end
